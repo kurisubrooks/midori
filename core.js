311e4910267c23dc55a9e36c7d80624123d29f4f
kurisubrooks <kurisubrooks@gmail.com>
2016-06-15 22:39:06 +1000

    Rename API Error Name

--- core.js
+++ core.js
@@ -1,10 +1,10 @@
 // nano²
 "use strict"
 
-const Discord = require("discord.js")
 const _ = require("lodash")
 const keys = require("./keys")
 const config = require("./config")
+const Discord = require("discord.js")
 const bot = new Discord.Client({
     autoReconnect: true
 })
@@ -15,27 +15,27 @@ module.exports = {
     post: function(channel, message, options) {
         if (options && options.author) message = `**[${options.author}]** ${message}`
         bot.sendMessage(channel, message, {}, (error, response) => {
-            if (error) this.error(response, "api")
+            if (error) this.error(response, "core")
         })
     },
     reply: function(message, reply) {
         bot.reply(message, reply, {}, (error, response) => {
-            if (error) this.error(response, "api")
+            if (error) this.error(response, "core")
         })
     },
     edit: function(message, update) {
         bot.updateMessage(message, update, {}, (error, response) => {
-            if (error) this.error(response, "api")
+            if (error) this.error(response, "core")
         })
     },
     delete: function(message) {
         bot.deleteMessage(message, {}, (error, response) => {
-            if (error) this.error(response, "api")
+            if (error) this.error(response, "core")
         })
     },
     upload: function(channel, file, text) {
         bot.sendFile(channel, file, "", text, (error, response) => {
-            if (error) this.error(response, "api")
+            if (error) this.error(response, "core")
         })
     },
     error: function(message, from) {
