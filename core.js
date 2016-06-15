4afb751f0e57dfa426f413ee18ce20f6009d2e40
kurisubrooks <kurisubrooks@gmail.com>
2016-06-16 01:04:57 +1000

    Post now takes an Object, Added Helper Prototype

--- core.js
+++ core.js
@@ -12,39 +12,56 @@ const bot = new Discord.Client({
 // Define API
 module.exports = {
     bot: bot,
-    post: function(channel, message, options) {
-        if (options && options.author) message = `**[${options.author}]** ${message}`
-        bot.sendMessage(channel, message, {}, (error, response) => {
+
+    post: function(data, callback) {
+        if (data.author) data.message = `**${data.author}**, ${data.message}`
+        bot.sendMessage(data.channel, data.message, {}, (error, response) => {
             if (error) this.error(response, "core")
+            if (callback) callback()
         })
     },
+
     reply: function(message, reply) {
         bot.reply(message, reply, {}, (error, response) => {
             if (error) this.error(response, "core")
         })
     },
+
     edit: function(message, update) {
         bot.updateMessage(message, update, {}, (error, response) => {
             if (error) this.error(response, "core")
         })
     },
+
     delete: function(message) {
         bot.deleteMessage(message, {}, (error, response) => {
             if (error) this.error(response, "core")
         })
     },
+
     upload: function(channel, file, text) {
         bot.sendFile(channel, file, "", text, (error, response) => {
             if (error) this.error(response, "core")
         })
     },
+
     error: function(message, from) {
+        if (typeof message === "object") message = JSON.stringify(message)
+
         console.log(`[Error in ${from}.js] ${message}`)
         _.each(config.debug, (channel) => {
-            this.post(channel, `**Error:** An error was thrown${(from) ? " from \`" + from + ".js\`" : ""}:\n\`\`\`${message}\`\`\``)
+            this.post({
+                channel: channel,
+                message: `**Error:** An error was thrown${(from) ? " from \`" + from + ".js\`" : ""}:\n\`\`\`${message}\`\`\``
+            })
         })
     }
 }
 
 // Initialise Discord
 bot.loginWithToken(keys.token)
+
+// Helper Prototypes
+String.prototype.toUpperLowerCase = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
