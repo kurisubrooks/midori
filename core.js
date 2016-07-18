f13e5ae188a932182691d63b65d5ed04ff782480
Kurisu Brooks <kurisubrooks@gmail.com>
2016-07-19 08:48:52 +1000

    Better Post formatting, Upload now takes an object, Updated error format

--- core.js
+++ core.js
@@ -14,54 +14,60 @@ module.exports = {
     bot: bot,
 
     post: function(data, callback) {
-        if (data.author) data.message = `**${data.author}**, ${data.message}`
-        bot.sendMessage(data.channel, data.message, {}, (error, response) => {
-            if (error) this.error(response, "core")
-            if (callback) callback()
+        if (data.author) data.message = `**[${data.author}]** ${data.message}`
+        if (!data.options || typeof data.options !== "object") data.options = {}
+        bot.sendMessage(data.channel, data.message, data.options, (error, response) => {
+            if (error) console.error("core.post: " + error)
+            if (callback) callback(error, response)
         })
     },
 
     reply: function(message, reply) {
         bot.reply(message, reply, {}, (error, response) => {
-            if (error) this.error(response, "core")
+            if (error) console.error("core.reply: " + error)
         })
     },
 
     edit: function(message, update) {
         bot.updateMessage(message, update, {}, (error, response) => {
-            if (error) this.error(response, "core")
+            if (error) console.error("core.edit: " + error)
         })
     },
 
     delete: function(message) {
         bot.deleteMessage(message, {}, (error, response) => {
-            if (error) this.error(response, "core")
+            if (error) console.error("core.delete: " + error)
         })
     },
 
-    upload: function(channel, file, text) {
-        bot.sendFile(channel, file, "", text, (error, response) => {
-            if (error) this.error(response, "core")
+    upload: function(data, callback) {
+        if (!data.text) data.text = ""
+        if (!data.name) data.name = ""
+
+        bot.sendFile(data.channel, data.file, data.name, data.text, (error, response) => {
+            if (error) console.error("core.upload: " + error)
+            if (callback) callback(error, response)
         })
     },
 
     error: function(message, from) {
         if (typeof message === "object") message = JSON.stringify(message)
 
-        console.log(`[Error in ${from}.js] ${message}`)
+        console.log(`[ERROR] ${from}.js - ${message}`)
+
         _.each(config.debug, (channel) => {
             this.post({
                 channel: channel,
-                message: `**Error:** An error was thrown${(from) ? " from \`" + from + ".js\`" : ""}:\n\`\`\`${message}\`\`\``
+                message: `**[ERROR]** \`${from}.js\`\n\`\`\`${message}\`\`\``
             })
         })
     }
 }
 
 // Initialise Discord
-bot.loginWithToken(keys.token)
+bot.loginWithToken(keys.discord)
 
 // Helper Prototypes
 String.prototype.toUpperLowerCase = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-};
+    return this.charAt(0).toUpperCase() + this.slice(1)
+}
