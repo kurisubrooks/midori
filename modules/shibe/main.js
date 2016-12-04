af2c121b48e976a2162aa58ae94e4f120b810bad
Kurisu Brooks <kurisubrooks@gmail.com>
2016-12-05 05:58:59 +1100

    Cleanup

--- modules/shibe/main.js
+++ modules/shibe/main.js
@@ -2,18 +2,14 @@
 
 const request = require("request")
 
-module.exports = (bot, channel, user, args, id, message, extra) => {
-    let util = extra.util
-    let url = `http://shibe.online/api/shibes?count=1&httpsurls=true`
-
-    request(url, (error, response, body) => {
+module.exports = (bot, channel, user, args, id, message, extra) =>
+    request(`http://shibe.online/api/shibes?count=1&httpsurls=true`, (error, response, body) => {
         if (error) {
-            util.error(error, "shibe", channel)
+            extra.util.error(error, "shibe", channel)
             return
         }
 
         channel.sendFile(JSON.parse(body)[0])
             .then(msg => message.delete())
-            .catch(error => util.error(error, "shibe", channel))
+            .catch(error => extra.util.error(error, "shibe", channel))
     })
-}
