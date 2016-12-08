9cfbd381700918250829dda6e50e5b9aae0d0878
Kurisu Brooks <kurisubrooks@gmail.com>
2016-12-08 18:05:14 +1100

    Adds Command to Trigger an Error

--- modules/admin/main.js
+++ modules/admin/main.js
@@ -10,23 +10,10 @@ module.exports = (bot, channel, user, args, id, message, extra) => {
         // Stop or Restart Bot
         if (args[0] === "stop") {
             channel.sendMessage("Restarting").then(setTimeout(() => process.exit(0), 500))
-
-            /*channel.sendFile("https://i.imgur.com/kiKRmYY.gif", null, "リスタート中、すぐに戻ります", (err, res) => {
-                if (err) util.error(err, "admin")
-                message.delete()
-                setTimeout(() => process.exit(0), 1000)
-            })*/
-
-        // Join or Change Voice Channels
-        } /*else if (args[0] === "voice") {
-            args[1].join((err, conn) => {
-                if (err) util.error(err, "admin")
-                if (conn) {
-                    channel.sendMessage("Connected to new Voice Channel")
-                    console.log(chalk.magenta.bold("Connected to new Voice Channel"))
-                }
-            })
-        }*/
+        // Trigger util.error
+        } else if (args[0] === "error") {
+            util.error("Error Triggered by Admin", "admin", channel)
+        }
 
     } else {
         channel.sendMessage("Insufficient Permissions")
