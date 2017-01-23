ec6941865f9e94177098a766ef52a60d0f61cd0b
Kurisu Brooks <kurisubrooks@gmail.com>
2017-01-24 05:50:34 +1100

    Query `^` to translate last message

--- modules/translate/main.js
+++ modules/translate/main.js
@@ -1,6 +1,5 @@
 "use strict"
 
-const _ = require("lodash")
 const qs = require("qs")
 const chalk = require("chalk")
 const request = require("request")
@@ -18,62 +17,72 @@ module.exports = (bot, channel, user, args, id, message, extra) => {
     let from = langs.length > 1 ? langs[1] : null
     let query = to === langs[0] ? args.slice(1).join(" ") : args.join(" ")
 
-    let params = {
-        to: to,
-        query: query
-    }
-
-    if (from) params.from = from
-
-    let fetch = {
-        headers: { "User-Agent": "Mozilla/5.0" },
-        url: "https://api.kurisubrooks.com/api/translate?" + qs.stringify(params)
-    }
-
-    request.get(fetch, (error, res, body) => {
-        if (error) {
-            util.error(error, "translate", channel)
-            return
+    let translate = (query) => {
+        let params = {
+            to: to,
+            query: query
         }
 
-        let response = JSON.parse(body)
-        let to       = response.to
-        let from     = response.from
-        let query    = response.query
-        let result   = response.result
+        if (from) params.from = from
 
-        if (response.ok) {
-            // Debug
-            console.log(chalk.magenta.bold("To:"), chalk.magenta(to))
-            console.log(chalk.magenta.bold("From:"), chalk.magenta(from))
-            console.log(chalk.magenta.bold("Query:"), chalk.magenta(query))
-            console.log(chalk.magenta.bold("Translation:"), chalk.magenta(result))
+        let fetch = {
+            headers: { "User-Agent": "Mozilla/5.0" },
+            url: "https://api.kurisubrooks.com/api/translate?" + qs.stringify(params)
+        }
 
-            let format = `${user}:\n**${from.name}**: ${query}\n**${to.name}**: ${result}`
-
-            let embed = {
-                color: extra.colours.default,
-                author: {
-                    name: extra.trigger.nickname,
-                    icon_url: extra.trigger.avatar
-                },
-                fields: [
-                    {
-                        name: from.name,
-                        value: query
-                    },
-                    {
-                        name: to.name,
-                        value: result
-                    }
-                ]
+        request.get(fetch, (error, res, body) => {
+            if (error) {
+                util.error(error, "translate", channel)
+                return
             }
 
-            channel.sendMessage("", { embed })
-                .then(() => message.delete())
-                .catch(error => util.error(error, "translate", channel))
-        } else {
-            util.error(response.error, "translate", channel)
-        }
-    })
+            let response = JSON.parse(body)
+            let to       = response.to
+            let from     = response.from
+            let query    = response.query
+            let result   = response.result
+
+            if (response.ok) {
+                // Debug
+                console.log(chalk.magenta.bold("To:"), chalk.magenta(to.name))
+                console.log(chalk.magenta.bold("From:"), chalk.magenta(from.name))
+                console.log(chalk.magenta.bold("Query:"), chalk.magenta(query))
+                console.log(chalk.magenta.bold("Translation:"), chalk.magenta(result))
+
+                let format = `${user}:\n**${from.name}**: ${query}\n**${to.name}**: ${result}`
+
+                let embed = {
+                    color: extra.colours.default,
+                    author: {
+                        name: extra.trigger.nickname,
+                        icon_url: extra.trigger.avatar
+                    },
+                    fields: [
+                        {
+                            name: from.name,
+                            value: query
+                        },
+                        {
+                            name: to.name,
+                            value: result
+                        }
+                    ]
+                }
+
+                channel.sendMessage("", { embed })
+                    .then(() => message.delete())
+                    .catch(error => util.error(error, "translate", channel))
+            } else {
+                util.error(response.error, "translate", channel)
+            }
+        })
+    }
+
+    if (query === "^") {
+        channel.fetchMessages({ before: id, limit: 1 })
+            .then(msg => translate(msg.first().content))
+            .catch(error => util.error(error, "translate", channel))
+    } else {
+        translate(query)
+    }
 }
