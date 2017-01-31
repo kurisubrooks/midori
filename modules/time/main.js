68b7591c509d0e7c80c2fbb271d1f4281f908764
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-01 08:45:46 +1100

    Respond with Embed

--- modules/time/main.js
+++ modules/time/main.js
@@ -1,10 +1,11 @@
 const request = require("superagent");
+const moment = require("moment");
 
 module.exports = (bot, channel, user, args, id, message, extra) => {
-    const { util } = extra;
+    const { util, config } = extra;
 
     if (args.length === 0 || (args.length === 1 && args[0] === "in")) {
-        return channel.send("Please specify a place you'd like to get the time of.");
+        return channel.send("Please specify a location or timezone");
     }
 
     return request
@@ -14,7 +15,29 @@ module.exports = (bot, channel, user, args, id, message, extra) => {
             const [, date] = text.match(/<div id="dd" class="w90 tr" onclick="location='\/calendar'" title=".+">(.+)<\/div><div id="daydiv/);
             const [, time] = text.match(/<div id="twd">(\d+:\d+:\d+)/);
 
-            channel.send(`${location} is now ${time}, ${date}`);
+            let embed = {
+                "color": config.colours.default,
+                "fields": [
+                    {
+                        "name": "Location",
+                        "value": location.replace("Time in ", "").replace(" now", "")
+                    },
+                    {
+                        "name": "Time, Date",
+                        "value": moment(time, "HH:mm:ss").format("h:mm a"),
+                        "inline": 1
+                    },
+                    {
+                        "name": "Date",
+                        "value": moment(date, "dddd, MMMM D, YYYY").format("ddd, MMM Do"),
+                        "inline": 1
+                    }
+                ]
+            };
+
+            channel.send("", { embed })
+                .then(() => message.delete())
+                .catch(error => util.error(error, "time", channel));
         })
         .catch(error => util.error(error.message, "time", channel));
 };
