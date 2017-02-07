f54c7a95c17c7a5361f108b10748f62e41fda353
Artful <kye.normangill@tafensw.net.au>
2017-02-07 20:39:46 +1100

    Fixed message id check (#6)
    * Fixed message id check

--- modules/translate/main.js
+++ modules/translate/main.js
@@ -78,7 +78,7 @@ module.exports = (bot, channel, user, args, id, message, extra) => {
         return channel.fetchMessages({ before: id, limit: 1 })
             .then(msg => translate(msg.first().content))
             .catch(error => util.error(error, "translate", channel));
-    } else if (Number(args[0])) {
+    } else if (Boolean(Number(args[0]))) {
         return channel.fetchMessage(args[0])
             .then(msg => translate(msg.content))
             .catch(error => util.error(error, "translate", channel));
