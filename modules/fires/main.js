eeaec838db0bc7fb1d637da26816e2d8eb956825
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-11 17:07:53 +1100

    Continuing Work on Fires Subprocess

--- modules/fires/main.js
+++ modules/fires/main.js
@@ -1,39 +1,53 @@
-"use strict"
+"use strict";
+
+const request = require("superagent");
 
 let levels = {
-    
-}
+    "Not Applicable": { url: "http://i.imgur.com/LzUSutH.png" },
+    "Advice": { url: "http://i.imgur.com/0bBmzZk.png" },
+    "Watch and Act": { url: "http://i.imgur.com/L37N9sN.png" },
+    "Emergency Warning": { url: "http://i.imgur.com/treI1DH.png" }
+};
 
 module.exports = (bot, util, config, keychain, dir) => {
-    let embed = {
-        "color": config.colours.error,
-        "author": {
-            "name": "Watch and Act",
-            "icon_url": "https://cdn.discordapp.com/attachments/187527424026607616/254960323662970881/FFD84A.png"
-        },
-        "fields": [
-            {
-                "name": "Location",
-                "value": "Castlereagh Road, Agnes Banks"
-            },
-
-            {
-                "name": "Type",
-                "value": "Grass Fire",
-                "inline": "1"
-            },
-            {
-                "name": "Status",
-                "value": "Out of control",
-                "inline": "1"
-            }
-        ],
-        "image": {
-            "url": "https://maps.googleapis.com/maps/api/staticmap?center=667%20Castlereagh%20Rd,%20Agnes%20Banks,%20NSW%202753&markers=667%20Castlereagh%20Rd,%20Agnes%20Banks,%20NSW%202753&size=400x220&zoom=12"
-        }
-    }
-
+    let channel = bot.channels.get("276249021579001857");
     let run = () => {
+        request.get("https://api.kurisubrooks.com/api/fire", (error, res, body) => {
+            if (error) {
+                return util.error(error, "fires");
+            } else if (res.statusCode !== 200) {
+                return util.error(`Request returned ${res.statusCode}`, "fires");
+            }
 
-    }
-}
+            let data = typeof body === "object" ? body : JSON.parse(body);
+            let embed = {
+                "color": config.colours.error,
+                "author": {
+                    "name": "Advice",
+                    "icon_url": "https://cdn.discordapp.com/attachments/273635911139328001/279818372051369994/unknown.png"
+                },
+                "fields": [
+                    {
+                        "name": "Location",
+                        "value": "Hickeys Lane, Penrith"
+                    },
+                    {
+                        "name": "Type",
+                        "value": "Grass Fire",
+                        "inline": 1
+                    },
+                    {
+                        "name": "Status",
+                        "value": "Out of control",
+                        "inline": 1
+                    }
+                ],
+                "image": { "url": "https://maps.googleapis.com/maps/api/staticmap?center=Hickeys%20Lane%20Penrith&markers=Hickeys%20Lane%20Penrith&size=400x220&zoom=12" }
+            };
+
+            return channel.send("", { embed }).catch(error => util.error(error, "fires"));
+        });
+    };
+
+    setInterval(() => run(), 1 * 60 * 1000);
+};
