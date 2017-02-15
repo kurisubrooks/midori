9fa8d1dbc89b597a34648311ff1ee9aee228c3f9
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-16 00:48:56 +1100

    Fix Lint Path, Copy Recursive Files

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const es_version = "es2017-node7";
 
 // Lint
 gulp.task("lint", () =>
-    gulp.src(["./**/*.js", "!./node_modules/*", "!./.git/*"])
+    gulp.src(["./*.js", "./modules/**/*.js"])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError())
@@ -24,6 +24,6 @@ gulp.task("build", () => {
     gulp.src("./modules/**/*.js")
         .pipe(babel({ presets: [es_version] }))
         .pipe(gulp.dest("bin/modules/"));
-    return gulp.src("./*.json")
+    return gulp.src(["./**/*.*", "!./node_modules/**/*", "!./.git/*", "!./**/*.js"])
         .pipe(gulp.dest("bin/"));
 });
