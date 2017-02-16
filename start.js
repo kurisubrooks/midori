b3e61f52480eee2642b60f3eef8be2aba4e65714
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-16 16:03:59 +1100

    Add start.js for pm2

---
+++ start.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: "midori",
+            script: "./bin/index.js",
+            args: "--color",
+            node_args: "--harmony"
+        }
+    ]
+};
