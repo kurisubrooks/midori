a8d05d886571c3e8c779693eb44915f5f06470a0
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-17 14:57:14 +1100

    Add Shibe

---
+++ commands/shibe/main.js
@@ -0,0 +1,23 @@
+import request from "request-promise";
+import Command from "../../core/Command";
+
+module.exports = class ShibeCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "shibe",
+            description: "Post a randomly selected image of a Shiba Inu",
+            aliases: ["shib", "doge"]
+        });
+    }
+
+    async run(message, channel) {
+        const response = await request({
+            uri: "http://shibe.online/api/shibes?count=1&httpsurls=true",
+            headers: { "User-Agent": "Mozilla/5.0" },
+            json: true
+        });
+
+        await channel.sendFile(response[0]);
+        return message.delete();
+    }
+};
