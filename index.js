29addbd166db054d07600bb049d34dd7d95371e1
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-18 03:34:07 +1100

    Add Subprocesses to Launch, Move handleReady to Index

--- index.js
+++ index.js
@@ -3,20 +3,25 @@
 
 // Core
 import { Client } from "discord.js";
-import { error, handleReady, handleJoin } from "./core/Util";
+import { log, error, handleJoin } from "./core/Util";
 import keychain from "./keychain.json";
 import CommandManager from "./core/CommandManager";
+import SubprocessManager from "./core/SubprocessManager";
 
 // Initialise
 const client = new Client();
+
+const Subprocesses = new SubprocessManager(client);
+Subprocesses.loadModules("./subprocesses/");
+
 const Manager = new CommandManager(client);
 Manager.loadCommands("./commands/");
 
 // Handle Discord
 client.login(keychain.discord);
-client.once("ready", () => handleReady(client));
-client.on("warn", warning => error(warning, null, "Core"));
-client.on("error", error => error(error, null, "Core"));
+client.once("ready", () => log("Discord", "Ready", "success"));
+client.on("warn", warning => error("Core", warning));
+client.on("error", error => error("Core", error));
 client.on("guildMemberAdd", member => handleJoin(member));
 client.on("message", message => Manager.handleMessage(message));
 
