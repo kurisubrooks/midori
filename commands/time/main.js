791ca5dfa1e4401189c246b36be36aa9559d5dae
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-18 14:18:52 +1100

    Add Time

---
+++ commands/time/main.js
@@ -0,0 +1,44 @@
+import moment from "moment";
+import cheerio from "cheerio";
+import request from "request-promise";
+import { RichEmbed } from "discord.js";
+import Command from "../../core/Command";
+
+export default class TimeCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "time",
+            description: "Get the Time for your Given Location",
+            aliases: ["t", "clock"]
+        });
+    }
+
+    async run(message, channel, user, args) {
+        if (args.length < 1 || (args.length === 1 && args[0] === "in")) {
+            return message.reply("Please provide a query");
+        }
+
+        const $ = await request({
+            uri: `http://time.is/${args.join(" ").replace(/^in/, "")}`,
+            headers: { "User-Agent": "Mozilla/5.0" },
+            transform: body => cheerio.load(body)
+        });
+
+        const place = $("#msgdiv > h1").text();
+        const date = $("#dd").text();
+        const time = $("#twd").text();
+
+        if (!place) {
+            return this.error("No Results", channel);
+        }
+
+        const embed = new RichEmbed()
+            .setColor(this.config.colours.default)
+            .addField("Location", place.replace("Time in ", "").replace(" now", ""))
+            .addField("Time", moment(`${time}`, "HH:mm:ssA").format("h:mm a"), true)
+            .addField("Date", date, true);
+
+        await channel.sendEmbed(embed);
+        return message.delete();
+    }
+}
