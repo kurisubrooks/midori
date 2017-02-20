8921a097a350deac7996f1584d4497207286bd28
Kurisu Brooks <kurisubrooks@users.noreply.github.com>
2017-02-21 00:59:48 +1100

    Fix Typo

--- core/Subprocess.js
+++ core/Subprocess.js
@@ -18,7 +18,7 @@ export default class Subprocess {
 
         if (!this.name) throw new Error("Subprocess Name is required");
         if (!this.description) throw new Error("Subprocess Description is required");
-        if (typeof this.disabled !== "boolean") throw new TypeError("Subprocess disbaled property must be a boolean");
+        if (typeof this.disabled !== "boolean") throw new TypeError("Subprocess disabled property must be a boolean");
         if (typeof this.name !== "string") throw new TypeError("Subprocess name must be a string");
         if (typeof this.description !== "string") throw new TypeError("Subprocess description must be a string");
         
