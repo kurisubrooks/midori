0b338c7445d04d5be76c18115dd61be8be4a68c8
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-22 00:17:07 +1100

    Fix Eval

--- commands/eval/main.js
+++ commands/eval/main.js
@@ -12,17 +12,19 @@ module.exports = class EvalCommand extends Command {
     async run(message, channel, user, args) {
         if (!this.hasAdmin(user)) return message.reply("Insufficient Permissions");
 
+        const bot = this.client, client = this.client; // eslint-disable-line no-unused-vars
+
         let input = `📥\u3000**Input:**\n\`\`\`js\n${args.join(" ")}\n\`\`\``;
         let error = err => `🚫\u3000**Error:**\n\`\`\`js\n${err.toString().replace(this.client.token, "<Token>")}\n\`\`\``;
 
         try {
             let output = eval(args.join(" "));
             if (typeof output !== "string") output = require("util").inspect(output, { depth: 1 });
-            let response = `📤\u3000**Output:**\n\`\`\`js\n${output.replace(this.client.token, "<Token>")}\n\`\`\``;
+            let response = `📤\u3000**Output:**\n\`\`\`js\n${output.replace(this.client.token, "[Token]")}\n\`\`\``;
             if (input.length + response.length > 1900) throw new Error("Output too long!");
-            return message.edit(`${input}\n${response}`).catch(err => message.edit(`${input}\n${error(err)}`));
+            return channel.send(`${input}\n${response}`).catch(err => channel.send(`${input}\n${error(err)}`));
         } catch(err) {
-            return message.edit(`${input}\n${error(err)}`).catch(err => message.edit(`${input}\n${error(err)}`));
+            return channel.send(`${input}\n${error(err)}`).catch(err => channel.send(`${input}\n${error(err)}`));
         }
     }
 };
