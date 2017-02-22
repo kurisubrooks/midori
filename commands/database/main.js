22f93a596a6ce0f2718892fd5af9b8aeb5577741
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-22 17:48:58 +1100

    Test Location before Saving

--- commands/database/main.js
+++ commands/database/main.js
@@ -1,3 +1,4 @@
+const request = require("superagent");
 const Command = require("../../core/Command");
 const { Users } = require("../../core/Models");
 
@@ -40,7 +41,34 @@ module.exports = class DatabaseCommand extends Command {
             let update = false;
 
             if (intention === "weather" || intention === "location") {
-                manipulate.weather = query;
+                let geolocation;
+                try {
+                    geolocation = await request
+                        .get(`https://maps.googleapis.com/maps/api/geocode/json`)
+                        .query(`address=${encodeURIComponent(query)}`)
+                        .query(`key=${this.keychain.google.geocode}`);
+                } catch(err) {
+                    this.log(err, "fatal", true);
+                    return this.error(err, channel);
+                }
+
+                if (geolocation.body.status !== "OK") return this.handleNotOK(channel, geolocation.body);
+                if (geolocation.body.results.length > 1) {
+                    let places = [];
+                    for (const val of geolocation.body.results) places.push(`\`${val.formatted_address}\``);
+                    return message.reply(`Too many results, please refine your search:\n${places.join(", ")}`);
+                }
+
+                const locality = geolocation.body.results[0].address_components.find(elem => elem.types.includes("locality"));
+                const governing = geolocation.body.results[0].address_components.find(elem => elem.types.includes("administrative_area_level_1"));
+                const country = geolocation.body.results[0].address_components.find(elem => elem.types.includes("country"));
+                const continent = geolocation.body.results[0].address_components.find(elem => elem.types.includes("continent"));
+
+                const city = locality || governing || country || continent || {};
+                const state = locality && governing ? governing : locality ? country : {};
+                const geocode = [geolocation.body.results[0].geometry.location.lat, geolocation.body.results[0].geometry.location.lng];
+
+                manipulate.weather = [city, state, geocode];
                 update = true;
             }
 
@@ -51,7 +79,7 @@ module.exports = class DatabaseCommand extends Command {
                 message.reply(`Updated Database successfully.`);
             }
 
-            return null;
+            return false;
         }
 
         // Remove or Delete a value from the DB
@@ -60,5 +88,19 @@ module.exports = class DatabaseCommand extends Command {
         } */
 
         return false;
+    }
+
+    handleNotOK(channel, geolocation) {
+        if (geolocation.status === "ZERO_RESULTS") {
+            return this.error("Query returned no results", channel);
+        } else if (geolocation.status === "REQUEST_DENIED") {
+            return this.error("Geocode API Request was denied", channel);
+        } else if (geolocation.status === "INVALID_REQUEST") {
+            return this.error("Invalid Request", channel);
+        } else if (geolocation.status === "OVER_QUERY_LIMIT") {
+            return this.error("Query Limit Exceeed, try again tomorrow.", channel);
+        } else {
+            return this.error("Unknown API Error", channel);
+        }
     }
 };
