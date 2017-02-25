345a35318f2ee3adf80df318b779049d0a2cf170
ItsJordan <ItsJordan@users.noreply.github.com>
2017-02-25 08:25:00 -0500

    Made util into a class.
    May or may not have just copy pasted the code from my desktop because I'm bad at git command line ._.

--- core/Util.js
+++ core/Util.js
@@ -4,82 +4,86 @@ const client = require("../index");
 const config = require("../config");
 const { RichEmbed } = require("discord.js");
 
+module.exports = class Util {
 // Logging Time Format
-const time = () => moment().format("HH:mm:ss");
+    static time() {
+        moment().format("HH:mm:ss");
+    }
 
-// String First Letter Upper Case
-const toUpper = exports.toUpper = string => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-};
+    // String First Letter Upper Case
+    static toUpper(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
 
-// Global Error Function
-exports.error = (name, message, channel) => {
-    const embed = new RichEmbed()
-        .setColor(config.colours.error)
-        .addField("Module", name, true)
-        .addField("Time", time(), true)
-        .addField("Message", message);
+    // Global Error Function
+    static error(name, message, channel) {
+        const embed = new RichEmbed()
+            .setColor(config.colours.error)
+            .addField("Module", name, true)
+            .addField("Time", this.time(), true)
+            .addField("Message", message);
 
-    channel = channel || client.channels.get("212917108445544449");
-    log(name, message, "error");
+        channel = channel || client.channels.get("212917108445544449");
+        this.log(name, message, "error");
 
-    return channel.sendEmbed(embed);
-};
+        return channel.sendEmbed(embed);
+    }
 
-// Global Logging Function
-const log = exports.log = (name, message, style, stacktrace) => {
-    let styles = {
-        default: chalk.white,
-        success: chalk.green,
-        warn: chalk.yellow,
-        error: chalk.red,
-        fatal: chalk.bgRed.white,
-        info: chalk.blue,
-        debug: chalk.magenta
-    };
+    // Global Logging Function
+    static log(name, message, style, stacktrace) {
+        let styles = {
+            default: chalk.white,
+            success: chalk.green,
+            warn: chalk.yellow,
+            error: chalk.red,
+            fatal: chalk.bgRed.white,
+            info: chalk.blue,
+            debug: chalk.magenta
+        };
 
-    style = style || "default";
+        style = style || "default";
 
-    if (Array.isArray(message)) {
-        for (const item of message) {
+        if (Array.isArray(message)) {
+            for (const item of message) {
+                console.log(
+                    styles[style].bold(`[${this.time()} ${this.toUpper(name)}]`),
+                    styles[style](item)
+                );
+            }
+
+            return false;
+        } else if (stacktrace) {
             console.log(
-                styles[style].bold(`[${time()} ${toUpper(name)}]`),
-                styles[style](item)
+                styles[style].bold(`[${this.time()} ${this.toUpper(name)}]`),
+                styles[style](message)
+            );
+
+            return console.trace(styles[style](message));
+        } else {
+            message = typeof message === "string" ? message.replace(/\r?\n|\r/g, " ") : message;
+            return console.log(
+                styles[style].bold(`[${this.time()} ${this.toUpper(name)}]`),
+                styles[style](message)
             );
         }
-
-        return false;
-    } else if (stacktrace) {
-        console.log(
-            styles[style].bold(`[${time()} ${toUpper(name)}]`),
-            styles[style](message)
-        );
-
-        return console.trace(styles[style](message));
-    } else {
-        message = typeof message === "string" ? message.replace(/\r?\n|\r/g, " ") : message;
-        return console.log(
-            styles[style].bold(`[${time()} ${toUpper(name)}]`),
-            styles[style](message)
-        );
     }
-};
 
-// Handle User Join
-exports.handleJoin = member => {
-    if (config.adminServer.includes(member.guild.id)) {
-        member.addRole(member.guild.roles.find("name", "Muggle"));
-        member.send("", { embed: { "description": "Welcome to Kurisu's Server!\nTo get started, I kindly ask you take the following quizzes,\nand post the results in #general, so you can be sorted in to your appropriate roles!\n \nhttps://my.pottermore.com/user-profile/my-house/ilvermorny \nhttps://my.pottermore.com/user-profile/my-house/hogwarts\n\nWe don't have a set series of rules as we're a relatively small server,\nbut I do kindly ask that you don't spam, be mature and don't troll.\n\nSincerely, Kurisu." } });
+    // Handle User Join
+    static handleJoin(member) {
+        if (config.adminServer.includes(member.guild.id)) {
+            member.addRole(member.guild.roles.find("name", "Muggle"));
+            member.send("", { embed: { "description": "Welcome to Kurisu's Server!\nTo get started, I kindly ask you take the following quizzes,\nand post the results in #general, so you can be sorted in to your appropriate roles!\n \nhttps://my.pottermore.com/user-profile/my-house/ilvermorny \nhttps://my.pottermore.com/user-profile/my-house/hogwarts\n\nWe don't have a set series of rules as we're a relatively small server,\nbut I do kindly ask that you don't spam, be mature and don't troll.\n\nSincerely, Kurisu." } });
+        }
     }
 };
 
 // Unhandled Promise Rejections
 process.on("unhandledRejection", reason =>
-    log("Unhandled Rejection", reason, "error", true));
+    this.log("Unhandled Rejection", reason, "error", true));
 
 // Unhandled Errors
 process.on("uncaughtException", error =>
-    log("Uncaught Exception", error, "error", true));
+    this.log("Uncaught Exception", error, "error", true));
 
 // Log Start
-log("Process", "Started", "info");
+this.log("Process", "Started", "info");
