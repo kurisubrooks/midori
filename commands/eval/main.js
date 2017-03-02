227e764c39ebf735e2ba778b89241caa1486501a
1Computer1 <mineox100@gmail.com>
2017-03-02 16:51:02 -0500

    Make eval token replace safer

--- commands/eval/main.js
+++ commands/eval/main.js
@@ -13,14 +13,15 @@ module.exports = class EvalCommand extends Command {
         if (!this.hasAdmin(user)) return message.reply("Insufficient Permissions");
 
         const bot = this.client, client = this.client; // eslint-disable-line no-unused-vars
+        const regex = new RegExp(this.client.token.replace(/\./g, "\\.").split("").join(".?"), "g");
 
         let input = `📥\u3000**Input:**\n\`\`\`js\n${args.join(" ")}\n\`\`\``;
-        let error = err => `🚫\u3000**Error:**\n\`\`\`js\n${err.toString().replace(this.client.token, "<Token>")}\n\`\`\``;
+        let error = err => `🚫\u3000**Error:**\n\`\`\`js\n${err.toString().replace(regex, "<Token>")}\n\`\`\``;
 
         try {
             let output = eval(args.join(" "));
             if (typeof output !== "string") output = require("util").inspect(output, { depth: 1 });
-            let response = `📤\u3000**Output:**\n\`\`\`js\n${output.replace(this.client.token, "[Token]")}\n\`\`\``;
+            let response = `📤\u3000**Output:**\n\`\`\`js\n${output.replace(regex, "[Token]")}\n\`\`\``;
             if (input.length + response.length > 1900) throw new Error("Output too long!");
             return channel.send(`${input}\n${response}`).catch(err => channel.send(`${input}\n${error(err)}`));
         } catch(err) {
