98e6e92f2bb7454e596ffc4644cb5e3118f7458d
Kurisu Brooks <kurisubrooks@gmail.com>
2017-04-02 00:28:27 +1100

    let → const where possible

--- commands/admin/eval.js
+++ commands/admin/eval.js
@@ -14,13 +14,13 @@ class Evaluator extends Command {
         const bot = this.client, client = this.client; // eslint-disable-line no-unused-vars
         const regex = new RegExp(this.client.token.replace(/\./g, "\\.").split("").join(".?"), "g");
 
-        let input = `📥\u3000**Input:**\n\`\`\`js\n${args.join(" ")}\n\`\`\``;
-        let error = err => `🚫\u3000**Error:**\n\`\`\`js\n${err.toString().replace(regex, "[Token]")}\n\`\`\``;
+        const input = `📥\u3000**Input:**\n\`\`\`js\n${args.join(" ")}\n\`\`\``;
+        const error = err => `🚫\u3000**Error:**\n\`\`\`js\n${err.toString().replace(regex, "[Token]")}\n\`\`\``;
 
         try {
             let output = eval(args.join(" "));
             if (typeof output !== "string") output = require("util").inspect(output, { depth: 1 });
-            let response = `📤\u3000**Output:**\n\`\`\`js\n${output.replace(regex, "[Token]")}\n\`\`\``;
+            const response = `📤\u3000**Output:**\n\`\`\`js\n${output.replace(regex, "[Token]")}\n\`\`\``;
             if (input.length + response.length > 1900) throw new Error("Output too long!");
             await channel.send(`${input}\n${response}`).catch(err => channel.send(`${input}\n${error(err)}`));
             return message.delete().catch(err => err.message);
