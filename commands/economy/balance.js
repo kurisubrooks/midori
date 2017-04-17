9e9ed053ff43b01f4f9ccaa9e9c326024d9b9b21
justin97530 <justin.hu.2002@gmail.com>
2017-04-17 17:02:44 +0530

    [WIP] Added rudimentary code for Economy.

---
+++ commands/economy/balance.js
@@ -0,0 +1,30 @@
+const Command = require("../../core/Command");
+const { RichEmbed } = require("discord.js");
+const Database = require("../../core/Database");
+
+class Embed extends Command {
+    constructor(client) {
+        super(client, {
+            name: "Balance",
+            description: "Get a user's balance.",
+            aliases: ["balance", "bal", "money"],
+            disabled: false
+        });
+    }
+
+    async run(message) {
+        const mentioned = message.mentions.users;
+        const user = mentioned.size > 0 ? mentioned.first() : message.author;
+        const data = JSON.parse((await Database.Models.Users.findOne({ where: { id: user.id } })).data);
+
+        const embed = new RichEmbed()
+            .setColor(this.config.colours.default)
+            .setAuthor(user.nickname, user.avatarURL)
+            .addField("Balance", data.balance === null ? 0 : data.balance);
+
+        await message.channel.send({ embed });
+        return message.delete().catch(err => err.message);
+    }
+}
+
+module.exports = Embed;
---
+++ commands/economy/pay.js
@@ -0,0 +1,61 @@
+const Command = require("../../core/Command");
+const { RichEmbed } = require("discord.js");
+const Database = require("../../core/Database");
+
+class Embed extends Command {
+    constructor(client) {
+        super(client, {
+            name: "Pay",
+            description: "Give another user some money.",
+            aliases: ["send", "give", "pay"],
+            disabled: false
+        });
+    }
+
+    async run(message, channel, user, args) {
+        const mentioned = message.mentions.users;
+
+        // No Args Supplied
+        if (mentioned.size === 0) {
+            return message.reply("Please provide a valid user.");
+        }
+
+        if (isNaN(args[1])) {
+            return message.reply("Please provide a valid number");
+        }
+
+        const amount = parseInt(args[1]);
+
+        const DB = await Database.Models.Users.findOne({ where: { id: user.id } });
+        const data = JSON.parse(DB.data);
+
+        if (data.balance > args[1]) {
+            return message.reply("You don't have enough balance!");
+        }
+
+        if (args[1] < 1) {
+            return message.reply("Amount must be greater than 1.");
+        }
+
+        const target = mentioned.first();
+        const targetDB = await Database.Models.Users.findOne({ where: { id: target.id } });
+        const targetData = JSON.parse(targetDB.data);
+
+        data.balance -= amount;
+        targetData.balance += amount;
+
+        await DB.update({ data: JSON.stringify(data) });
+        await targetDB.update({ data: JSON.stringify(targetData) });
+
+        const embed = new RichEmbed()
+            .setColor(this.config.colours.default)
+            .setAuthor(user.nickname, user.avatarURL)
+            .addField("Paid", amount)
+            .addField("Balance", data.balance);
+
+        await message.channel.send({ embed });
+        return message.delete().catch(err => err.message);
+    }
+}
+
+module.exports = Embed;
