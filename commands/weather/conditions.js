b97633964d0c88db427643d9dab7115040f8c9ce
Kurisu Brooks <kurisubrooks@gmail.com>
2017-04-21 23:17:11 +1000

    Weather v4

--- commands/weather/conditions.js
+++ commands/weather/conditions.js
@@ -1,5 +1,7 @@
+/* eslint-disable complexity */
 const path = require("path");
 const Canvas = require("canvas");
+const moment = require("moment-timezone");
 const request = require("request-promise");
 const Command = require("../../core/Command");
 const Database = require("../../core/Database");
@@ -66,10 +68,26 @@ class Weather extends Command {
                 return message.reply(`Too many results, please refine your search:\n${places.join(", ")}`);
             }
 
-            const locality = geolocation.results[0].address_components.find(elem => elem.types.includes("locality"));
-            const governing = geolocation.results[0].address_components.find(elem => elem.types.includes("administrative_area_level_1"));
-            const country = geolocation.results[0].address_components.find(elem => elem.types.includes("country"));
-            const continent = geolocation.results[0].address_components.find(elem => elem.types.includes("continent"));
+            const place = geolocation.results[0].address_components;
+            const locality = place.find(elem => elem.types.includes("locality"));
+            const governing = place.find(elem => elem.types.includes("administrative_area_level_1"));
+            const country = place.find(elem => elem.types.includes("country"));
+            const continent = place.find(elem => elem.types.includes("continent"));
+
+            console.log(place);
+
+            /*
+            if natural_feature -> +natural_feature
+            elseif point_of_interest -> +point_of_interest
+            elseif locality -> +locality
+            elseif ward -> +ward
+            else
+                if administrative_area_level_3 -> +administrative_area_level_3,
+                if administrative_area_level_2 -> +administrative_area_level_2
+
+            if administrative_area_level_1 -> +administrative_area_level_1
+            if country -> +country
+            */
 
             city = locality || governing || country || continent || {};
             state = locality && governing ? governing : locality ? country : {};
@@ -83,43 +101,38 @@ class Weather extends Command {
             headers: { "User-Agent": "Mozilla/5.0" },
             uri: `https://api.darksky.net/forecast/${this.keychain.darksky}/${geocode.join(",")}`,
             json: true,
-            qs: { units: "si" }
+            qs: { units: "si", excludes: "minutely,hourly,alerts" }
         }).catch(error => this.error(error.response.body.error, channel));
 
         if (!weather) return false;
+
+        // console.log(weather);
+
+        const locale = weather.flags.units === "us" ? "F" : "C";
         const condition = weather.currently.summary;
         const icon = weather.currently.icon;
-        const chanceofrain = Math.round((weather.currently.precipProbability * 100) / 5) * 5;
         const temperature = Math.round(weather.currently.temperature);
-        const humidity = Math.round(weather.currently.humidity * 100);
+        const datetime = moment().tz(weather.timezone).format("h:mma");
+        const forecast = weather.daily.data;
 
-        this.log(`${temperature}°C, ${condition}, ${humidity}%`, "debug");
+        this.log(`${temperature}°${locale}, ${condition}`, "debug");
 
-        Canvas.registerFont(path.join(__dirname, "fonts", "Roboto-Regular.ttf"), { family: "Roboto" });
-        Canvas.registerFont(path.join(__dirname, "fonts", "RobotoCondensed-Regular.ttf"), { family: "Roboto Condensed" });
-        Canvas.registerFont(path.join(__dirname, "fonts", "RobotoMono-Light.ttf"), { family: "Roboto Mono" });
+        Canvas.registerFont(path.join(__dirname, "fonts", "Roboto.ttf"), { family: "Roboto" });
+        Canvas.registerFont(path.join(__dirname, "fonts", "NexaBold.otf"), { family: "Nexa Bold" });
 
         // Generate Response Image
-        const canvas = new Canvas(400, 180);
+        const canvas = new Canvas(400, 250);
         const ctx = canvas.getContext("2d");
         const { Image } = Canvas;
         const base = new Image();
         const cond = new Image();
-        const humid = new Image();
-        const precip = new Image();
-
-        let theme = "light";
-        let fontColour = "#FFFFFF";
-
-        if (this.getBaseImage(icon) === "snow") {
-            theme = "dark";
-            fontColour = "#444444";
-        }
+        const day1 = new Image();
+        const day2 = new Image();
 
         base.src = path.join(__dirname, "base", `${this.getBaseImage(icon)}.png`);
-        cond.src = path.join(__dirname, "icons", theme, `${icon}.png`);
-        humid.src = path.join(__dirname, "icons", theme, "humidity.png");
-        precip.src = path.join(__dirname, "icons", theme, "precip.png");
+        cond.src = path.join(__dirname, "icons", `${this.getConditionImage(icon)}.png`);
+        day1.src = path.join(__dirname, "icons", `${this.getConditionImage(forecast[1].icon)}.png`);
+        day2.src = path.join(__dirname, "icons", `${this.getConditionImage(forecast[2].icon)}.png`);
 
         // Environment Variables
         ctx.drawImage(base, 0, 0);
@@ -130,56 +143,86 @@ class Weather extends Command {
 
         // City Name
         ctx.font = "20px Roboto";
-        ctx.fillStyle = fontColour;
-        ctx.fillText(city.long_name ? city.long_name : "Unknown", 35, 50);
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText(city.long_name ? city.long_name : "Unknown", 28, 40);
 
         // State/Prefecture Name
         ctx.font = "16px Roboto";
-        ctx.fillStyle = theme === "light" ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)";
-        ctx.fillText(state.long_name ? state.long_name : "", 35, 72.5);
+        ctx.fillStyle = "rgba(255, 255, 255, 0.75)";
+        ctx.fillText(state.long_name ? state.long_name : "", 28, 63);
 
         // Temperature
-        ctx.font = "48px 'Roboto Mono'";
-        ctx.fillStyle = fontColour;
-        ctx.fillText(`${temperature}°`, 35, 140);
+        ctx.font = "bold 34px 'Nexa Bold'";
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText(`${temperature}°C`, 28, 146);
 
-        // Condition
+        // Local Time
         ctx.textAlign = "right";
         ctx.font = "16px Roboto";
-        ctx.fillText(condition, 370, 142);
+        ctx.fillStyle = "rgba(255, 255, 255, 0.75)";
+        ctx.fillText(datetime, 375, 122);
 
-        // Images
-        ctx.drawImage(cond, 325, 31);
-        ctx.drawImage(humid, 358, 88);
-        ctx.drawImage(precip, 358, 108);
+        // Condition
+        ctx.font = "16px Roboto";
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText(condition, 375, 145);
+        ctx.drawImage(cond, 318, 20);
 
-        ctx.font = "16px 'Roboto Condensed'";
-        ctx.fillText(`${humidity}%`, 353, 100);
-        ctx.fillText(`${chanceofrain}%`, 353, 121);
+        // Forecast Day 1
+        ctx.textAlign = "left";
+        ctx.font = "16px Roboto";
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText(moment.unix(forecast[1].time).format("dddd"), 28, 193);
+        ctx.textAlign = "right";
+        ctx.fillText(`${Math.round(forecast[1].temperatureMax)}°${locale}`, 340, 193);
+        ctx.drawImage(day1, 350, 175, 24, 24);
+
+        // Forecast Day 2
+        ctx.textAlign = "left";
+        ctx.font = "16px Roboto";
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText(moment.unix(forecast[2].time).format("dddd"), 28, 220);
+        ctx.textAlign = "right";
+        ctx.fillText(`${Math.round(forecast[2].temperatureMax)}°${locale}`, 340, 220);
+        ctx.drawImage(day2, 350, 203, 24, 24);
 
         // Send
         await channel.sendFile(canvas.toBuffer());
-        return message.delete().catch(err => err.message);
+        return this.delete(message);
+    }
+
+    // Get Image
+    getConditionImage(input) {
+        const icons = {
+            "clear-day": "Day",
+            "clear-night": "Night",
+            "cloudy": "Cloudy",
+            "flurries": "Snow",
+            "fog": "Particles",
+            "partly-cloudy-day": "Day_Partly_Cloudy",
+            "partly-cloudy-night": "Night_Partly_Cloudy",
+            "rain": "Rain",
+            "sleet": "Snow",
+            "snow": "Snow",
+            "thunderstorm": "Thunderstorm",
+            "unknown": "Unknown"
+        };
+
+        return icons[input] || "Unknown";
     }
 
     // Get Background Image based on Weather Condition
     getBaseImage(input) {
-        if (input === "clear-day" || input === "partly-cloudy-day") {
+        if (input === "clear-day" || input === "partly-cloudy-day" || input === "cloudy") {
             return "day";
         } else if (input === "clear-night" || input === "partly-cloudy-night") {
             return "night";
-        } else if (input === "rain") {
+        } else if (input === "rain" || input === "thunderstorm") {
             return "rain";
-        } else if (input === "thunderstorm") {
-            return "night";
-        } else if (input === "snow" || input === "sleet" || input === "fog") {
+        } else if (input === "snow" || input === "sleet" || input === "flurries") {
             return "snow";
-        } else if (input === "wind" || input === "tornado") {
-            return "windy";
-        } else if (input === "cloudy") {
-            return "cloudy";
         } else {
-            return "cloudy";
+            return "day";
         }
     }
 
