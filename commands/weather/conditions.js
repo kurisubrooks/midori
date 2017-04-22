15d215449f83e3e584469895e8490c7d5dff02c0
Kurisu Brooks <kurisubrooks@gmail.com>
2017-04-22 15:50:23 +1000

    New Location Parser

--- commands/weather/conditions.js
+++ commands/weather/conditions.js
@@ -16,7 +16,7 @@ class Weather extends Command {
     }
 
     async run(message, channel, user, args) {
-        let geolocation, city, state, geocode;
+        let geolocation, line1, found1, line2, geocode;
 
         // No Args Supplied
         if (args.length === 0) {
@@ -29,12 +29,11 @@ class Weather extends Command {
 
                 // Checks if User has a set location
                 if (data.weather || data.location) {
-                    console.log(data.weather);
-                    city = data.weather[0];
-                    state = data.weather[1];
+                    line1 = data.weather[0].long_name;
+                    line2 = data.weather[1].long_name;
                     geocode = data.weather[2];
                     geolocation = true;
-                    this.log(`Using Cached Geolocation`, "debug");
+                    this.log(`Using Cached Geolocation (${line1}, ${line2})`, "debug");
                 } else {
                     return message.reply(err);
                 }
@@ -61,36 +60,55 @@ class Weather extends Command {
             if (geolocation.results.length > 1) {
                 let places = [];
 
+                this.log("Too Many Results", "debug");
+
                 for (const val of geolocation.results) {
                     places.push(`\`${val.formatted_address}\``);
                 }
 
-                return message.reply(`Too many results, please refine your search:\n${places.join(", ")}`);
+                return message.reply(`Too many results were returned!\nHere's some of the returned results, please try to narrow it down for me...\n${places.join(", ")}`);
             }
 
             const place = geolocation.results[0].address_components;
-            const locality = place.find(elem => elem.types.includes("locality"));
-            const governing = place.find(elem => elem.types.includes("administrative_area_level_1"));
-            const country = place.find(elem => elem.types.includes("country"));
-            const continent = place.find(elem => elem.types.includes("continent"));
+            const find = locality => place.find(elem => elem.types.includes(locality));
 
-            console.log(place);
+            if (find("neighborhood")) {
+                line1 = find("neighborhood").long_name;
+            } else if (find("natural_feature")) {
+                line1 = find("natural_feature").long_name;
+            } else if (find("point_of_interest")) {
+                line1 = find("point_of_interest").long_name;
+            } else if (find("locality")) {
+                line1 = find("locality").long_name;
+            } else if (find("ward")) {
+                line1 = find("ward").long_name;
+            } else if (find("administrative_area_level_3")) {
+                line1 = find("administrative_area_level_3").long_name;
+            } else if (find("administrative_area_level_2")) {
+                line1 = find("administrative_area_level_2").long_name;
+            } else if (find("administrative_area_level_1")) {
+                line1 = find("administrative_area_level_1").long_name;
+                found1 = 1;
+            } else if (find("country")) {
+                line1 = find("country").long_name;
+                found1 = 2;
+            } else if (find("continent")) {
+                line1 = find("continent").long_name;
+                found1 = 3;
+            } else {
+                line1 = "Unknown";
+            }
 
-            /*
-            if natural_feature -> +natural_feature
-            elseif point_of_interest -> +point_of_interest
-            elseif locality -> +locality
-            elseif ward -> +ward
-            else
-                if administrative_area_level_3 -> +administrative_area_level_3,
-                if administrative_area_level_2 -> +administrative_area_level_2
+            if (find("administrative_area_level_1") && line1 !== find("administrative_area_level_1").long_name && found1 !== 1) {
+                line2 = find("administrative_area_level_1").long_name;
+            } else if (find("country") && line1 !== find("country").long_name && found1 !== 2) {
+                line2 = find("country").long_name;
+            } else if (find("continent") && line1 !== find("continent").long_name && found1 !== 3) {
+                line2 = find("continent").long_name;
+            } else {
+                line2 = "";
+            }
 
-            if administrative_area_level_1 -> +administrative_area_level_1
-            if country -> +country
-            */
-
-            city = locality || governing || country || continent || {};
-            state = locality && governing ? governing : locality ? country : {};
             geocode = [geolocation.results[0].geometry.location.lat, geolocation.results[0].geometry.location.lng];
 
             this.log(`Geolocation Retrieved`, "debug");
@@ -115,7 +133,7 @@ class Weather extends Command {
         const datetime = moment().tz(weather.timezone).format("h:mma");
         const forecast = weather.daily.data;
 
-        this.log(`${temperature}°${locale}, ${condition}`, "debug");
+        this.log(`${line1}, ${line2}: ${temperature}°${locale}, ${condition}, ${datetime}`, "debug");
 
         Canvas.registerFont(path.join(__dirname, "fonts", "Roboto.ttf"), { family: "Roboto" });
         Canvas.registerFont(path.join(__dirname, "fonts", "Rubik.ttf"), { family: "Rubik" });
@@ -144,12 +162,12 @@ class Weather extends Command {
         // City Name
         ctx.font = "20px Roboto";
         ctx.fillStyle = "#FFFFFF";
-        ctx.fillText(city.long_name ? city.long_name : "Unknown", 28, 40);
+        ctx.fillText(line1, 28, 40);
 
         // State/Prefecture Name
         ctx.font = "16px Roboto";
         ctx.fillStyle = "rgba(255, 255, 255, 0.75)";
-        ctx.fillText(state.long_name ? state.long_name : "", 28, 63);
+        ctx.fillText(line2, 28, 63);
 
         // Temperature
         ctx.font = "bold 34px Rubik";
