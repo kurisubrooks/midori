06c0a68c958217a9b0f4630ad90eb531fd0749af
Kurisu Brooks <kurisubrooks@gmail.com>
2017-04-26 00:17:25 +1000

    Minor Tweaks

---
+++ commands/converters/halfwidth.js
@@ -0,0 +1,21 @@
+const Command = require("../../core/Command");
+
+class HalfWidthConverter extends Command {
+    constructor(client) {
+        super(client, {
+            name: "HalfWidth",
+            description: "Convert Full Width Latin to Half Width",
+            aliases: ["hw"]
+        });
+    }
+
+    async run(message, channel, user, args) {
+        const fw = "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ　ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ０１２３４５６７８９／－＋～！＠＃＄％＾＆＊（）＿＋、。";
+        const hw = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/-+~!@#$%^&*()_+,.";
+        const input = args.join(" ").split("");
+        const output = input.map(letter => fw.indexOf(letter) > -1 ? hw[fw.indexOf(letter)] : letter).join("");
+        channel.sendMessage(output);
+    }
+}
+
+module.exports = HalfWidthConverter;
