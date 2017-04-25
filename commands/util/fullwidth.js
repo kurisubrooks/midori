8f51fa4be1bb356650a8580759cd2bb538e07263
Kurisu Brooks <kurisubrooks@gmail.com>
2017-04-25 23:37:26 +1000

    Add Fullwidth Converter

---
+++ commands/util/fullwidth.js
@@ -0,0 +1,21 @@
+const Command = require("../../core/Command");
+
+class FullWidthConverter extends Command {
+    constructor(client) {
+        super(client, {
+            name: "FullWidth",
+            description: "Convert Half Width Latin to Full Width",
+            aliases: ["fw"]
+        });
+    }
+
+    async run(message, channel, user, args) {
+        const fw = "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ　ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ０１２３４５６７８９／－＋～！＠＃＄％＾＆＊（）＿＋";
+        const hw = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/-+~!@#$%^&*()_+";
+        const input = args.join(" ").split("");
+        const output = input.map(letter => hw.indexOf(letter) > -1 ? fw[hw.indexOf(letter)] : letter).join("");
+        channel.sendMessage(output);
+    }
+}
+
+module.exports = FullWidthConverter;
